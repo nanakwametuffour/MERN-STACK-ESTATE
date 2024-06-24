3f104529acc954912d129905b77f950cd93065ed
nanakwametuffour <nanakwametuffour44@gmail.com>
2024-06-24 03:17:12 -0700

    add pre installation

--- api/index.js
+++ api/index.js
@@ -3,11 +3,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
+import path from "path";
 
 dotenv.config();
-
-const app = express();
-app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO)
@@ -18,6 +16,11 @@ mongoose
     console.log("Not connected to mongodb");
   });
 
+const __dirname = path.resolve();
+
+const app = express();
+app.use(express.json());
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
@@ -27,12 +30,18 @@ app.listen(5000, () => {
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode|| 500;
-   const message = err.message || "Internal Server Error";
-   return res.status(statusCode).json({
-      success: false,
-       statusCode,
-       message,
-   })
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
