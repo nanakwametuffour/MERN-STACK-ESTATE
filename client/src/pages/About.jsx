e120d8b070ae35471f09947acef20237c43291e8
nanakwametuffour <nanakwametuffour44@gmail.com>
2024-07-01 15:21:44 -0700

    complete the about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About kwame Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Kwame Estate lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus eaque
+        itaque voluptatibus recusandae earum nesciunt, odit illo voluptatum
+        odio, repudiandae laudantium, fugit harum quis expedita! Harum libero
+        quae officiis necessitatibus.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus eaque
+        itaque voluptatibus recusandae earum nesciunt, odit illo voluptatum
+        odio, repudiandae laudantium, fugit harum quis expedita! Harum libero
+        quae officiis necessitatibus.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus eaque
+        itaque voluptatibus recusandae earum nesciunt, odit illo voluptatum
+        odio, repudiandae laudantium, fugit harum quis expedita! Harum libero
+        quae officiis necessitatibus.
+      </p>
+    </div>
+  );
 }
-
